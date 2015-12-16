cc4d671c8d7365c49122c23a9a957e5c2b41406a
Jørgen <jorgenjr11@gmail.com>
2015-12-16 12:50:14 +0100

    Bug fixed: needed to copy variable, not reference to it

--- programming/simulation_CIBERSORT/file_handler.py
+++ programming/simulation_CIBERSORT/file_handler.py
@@ -61,47 +61,47 @@ def read_files(INPUT):
 	print(np_gene_dictionary)
 
 
-def write_to_file(BEGIN, END, normalized_matrix, INPUT, OUTPUT):
+# def write_to_file(BEGIN, END, normalized_matrix, INPUT, OUTPUT):
 
-	""" Creating new files with the quantile normalized matrix with added noise.
-	Everything from the original data is copied except the MEAN value, which is from the normalized matrix.
-	"""
+# 	""" Creating new files with the quantile normalized matrix with added noise.
+# 	Everything from the original data is copied except the MEAN value, which is from the normalized matrix.
+# 	"""
 
-	f1 = open('../../../Master_files/input/' + INPUT[0], 'r')
-	f2 = open('../../../Master_files/input/' + INPUT[1], 'r')
-	f1_new = open('../../../Master_files/output/' + OUTPUT[0], 'w')
-	f2_new = open('../../../Master_files/output/' + OUTPUT[1], 'w')
+# 	f1 = open('../../../Master_files/input/' + INPUT[0], 'r')
+# 	f2 = open('../../../Master_files/input/' + INPUT[1], 'r')
+# 	f1_new = open('../../../Master_files/output/' + OUTPUT[0], 'w')
+# 	f2_new = open('../../../Master_files/output/' + OUTPUT[1], 'w')
 
-	insert = 0
+# 	insert = 0
 
-	for x in range(0, END):
+# 	for x in range(0, END):
 
-		if x < BEGIN:
+# 		if x < BEGIN:
 
-			f1_new.write(linecache.getline('../../../Master_files/input/' + INPUT[0], x))
-			f2_new.write(linecache.getline('../../../Master_files/input/' + INPUT[1], x))
+# 			f1_new.write(linecache.getline('../../../Master_files/input/' + INPUT[0], x))
+# 			f2_new.write(linecache.getline('../../../Master_files/input/' + INPUT[1], x))
 
-		elif x < END:
+# 		elif x < END:
 
-			line1 = linecache.getline('../../../Master_files/input/' + INPUT[0], x)
-			line2 = linecache.getline('../../../Master_files/input/' + INPUT[1], x)
+# 			line1 = linecache.getline('../../../Master_files/input/' + INPUT[0], x)
+# 			line2 = linecache.getline('../../../Master_files/input/' + INPUT[1], x)
 
-			list1 = np.array(line1.split('\t'))
-			list2 = np.array(line2.split('\t'))
+# 			list1 = np.array(line1.split('\t'))
+# 			list2 = np.array(line2.split('\t'))
 
-			list1 = [list1[GENE], normalized_matrix[x-BEGIN][0]]
-			list2 = [list1[GENE], normalized_matrix[x-BEGIN][1]]
+# 			list1 = [list1[GENE], normalized_matrix[x-BEGIN][0]]
+# 			list2 = [list1[GENE], normalized_matrix[x-BEGIN][1]]
 
-			f1_new.write('\t'.join(str(s) for s in list1) + '\n')
-			f2_new.write('\t'.join(str(s) for s in list2) + '\n')
+# 			f1_new.write('\t'.join(str(s) for s in list1) + '\n')
+# 			f2_new.write('\t'.join(str(s) for s in list2) + '\n')
 
-		else:
-			print (linecache.getline('../../../Master_files/input/' + INPUT[0], x))
-			print (linecache.getline('../../../Master_files/input/' + INPUT[1], x))
+# 		else:
+# 			print (linecache.getline('../../../Master_files/input/' + INPUT[0], x))
+# 			print (linecache.getline('../../../Master_files/input/' + INPUT[1], x))
 		
-		insert += 1
+# 		insert += 1
 
-def test_write(NP_GENE_DICTIONARY, FILE_NUMBER):
+def write_to_file(NP_GENE_DICTIONARY, FILE_NUMBER):
 
 	f1 = open('../../../Master_files/output/testfile_' + str(FILE_NUMBER), 'w')
 
