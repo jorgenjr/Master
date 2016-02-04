979ca8cd1aecbcd15c4d22350ad07bd4b0f992c9
Jørgen <jorgenjr11@gmail.com>
2016-02-04 12:14:47 +0100

    QA from_gsm_to_geneid.py

---
+++ programming/convert_geneID/from_gsm_to_geneid.py
@@ -0,0 +1,29 @@
+
+def from_gsm_to_geneid():
+
+	""" Opens a GSM file, retrieves the gene IDs, and writes the IDs to a new file.
+	"""
+
+	f_read = open('../../../Master_files/simulation/testfile_1_1', 'r')
+	f_write = open('../../../Master_files/convert/testfile_1_1_geneid', 'w')
+	header = True
+	gene_ids = []
+
+	for line in f_read:
+
+		if (header == True):
+			header = False
+			continue
+
+		splitted_line = line.split('\t')
+		gene_ids.append(splitted_line[0])	
+
+	gene_ids.sort()
+
+	for i in range(len(gene_ids)):
+
+		f_write.write(gene_ids[i] + '\n')
+
+	f_write.close()
+
+from_gsm_to_geneid()
