020c5c2b0972253a7931e2fadb9113159abf5216
Jørgen <jorgenjr11@gmail.com>
2016-02-04 12:05:31 +0100

    QA compare.py

--- programming/convert_geneID/compare.py
+++ programming/convert_geneID/compare.py
@@ -1,52 +1,76 @@
 
 import time
 
-f_lm = open('../../../Master_files/convert/LM22.txt', 'r')
-f_hugo = open('../../../Master_files/diff_exp/hugo', 'r')
-f_matches = open('matches', 'w')
+def get_gene_from_lm22():
 
-lm = []
-hugo = []
-match = 0
-unique_match = []
-skip = True
+	""" Reads LM22.txt and retrieves the gene ID from it.
+	"""
 
-for line in f_lm:
-	
-	if (skip):
-		skip = False
-		continue;
+	f_lm = open('../../../Master_files/convert/LM22.txt', 'r')
+	skip = True
+	lm = []
 
-	splitted_line = line.split('\t')
-	lm.append(splitted_line[0])
-	
-skip = True
+	for line in f_lm:
+		
+		if (skip == True):
+			skip = False
+			continue;
 
-for line in f_hugo:
+		splitted_line = line.split('\t')
+		lm.append(splitted_line[0])
+		
+	f_lm.close()
 
-	if (skip):
-		skip = False
-		continue;
+	return lm
 
-	splitted_line = line.split('\t')
-	hugo_symbol = splitted_line[2].split('"')
-	hugo.append(hugo_symbol[1])
-	
-	if (hugo_symbol[1] in lm):
-		match += 1
+def compare_against_lm22(lm):
 
-		if (hugo_symbol[1] not in unique_match):
-			unique_match.append(hugo_symbol[1])
+	""" Reads hugo, retrieves gene ID from it, and compares it to genes from LM22.
+	If a match, then the variable 'match' is incremented.
+	If a match, but not found earlier, then added to 'unique_match' list.
+	"""
+
+	f_hugo = open('../../../Master_files/diff_exp/hugo', 'r')
+	skip = True
+	match = 0
+	unique_match = []
+
+	for line in f_hugo:
+
+		if (skip == True):
+			skip = False
+			continue;
+
+		splitted_line = line.split('\t')
+		hugo_symbol = splitted_line[2].split('"')
+		
+		if (hugo_symbol[1] in lm):
+			match += 1
+
+			if (hugo_symbol[1] not in unique_match):
+				unique_match.append(hugo_symbol[1])
 			
 
-unique_match.sort()
+	unique_match.sort()
+	f_hugo.close()
+	
+	return match, unique_match
 
-f_matches.write("Total matches: " + str(match) + '\n')
-f_matches.write("Unique gene matches: " + str(len(unique_match)) + '\n')
+def write_matches_to_file(match, unique_match):
 
-for i in range(len(unique_match)):
-	f_matches.write(unique_match[i] + "\n")
+	""" Write total matches, total unique matches, and all unique genes to file.
+	"""
 
-f_matches.close()
-f_lm.close()
-f_hugo.close()
+	f_matches = open('../../../Master_files/convert/matches', 'w')
+
+	f_matches.write("Total matches: " + str(match) + '\n')
+	f_matches.write("Unique gene matches: " + str(len(unique_match)) + '\n')
+
+	for i in range(len(unique_match)):
+		f_matches.write(unique_match[i] + "\n")
+
+	f_matches.close()
+
+lm = get_gene_from_lm22()
+match, unique_match = compare_against_lm22(lm)
+write_matches_to_file(match, unique_match)
