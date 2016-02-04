Bug in quantile normalisation discovered. Need a fix ASAP! Or run it in R.

programming/simulation_CIBERSORT/mixtures.py
@@ -14,7 +14,7 @@ def combine_cell_line(BEGIN, END, FILECOLS, INPUT, GENE_DICTIONARY):
 
 	for x in range(BEGIN, END):
 
-		line = linecache.getline('../../../Master_files/input/' + INPUT, x)
+		line = linecache.getline('../../../Master_files/simulation/' + INPUT, x)
 		line_list = np.array(line.split('\t'))
 
 		Jurkat = 0.0
@@ -84,12 +84,25 @@ def from_dictionary_to_matrix(GENE_DICTIONARY):
 	""" Convert the dictionary containing genes to a matrix
 	"""
 
-	np_matrix_combined = np.zeros(shape=(len(GENE_DICTIONARY), 5))
+	value_length = 0
+
+	for key, value in GENE_DICTIONARY.items():
+		print("2: ", key, ": ", value)
+		value_length = len(value)
+		break;
+
+	np_matrix_combined = np.zeros(shape=(len(GENE_DICTIONARY), value_length))
 	insert = 0
 
 	for key, value in GENE_DICTIONARY.items():
+
+		value_list = []
+
+		for i in range(len(value)):
+			value_list.append(value[i])
 		
-		np_matrix_combined[insert] = np.array([value[0], value[1], value[2], value[3], value[4]])
+		np_matrix_combined[insert] = np.array(value_list)
+		#np_matrix_combined[insert] = np.array([value[0], value[1], value[2], value[3], value[4]])
 		insert += 1
 
 	return np_matrix_combined
@@ -100,11 +113,19 @@ def from_matrix_to_dictionary(COMBINED, GENE_DICTIONARY):
 	""" Convert the matrix containing genes to a dictionary
 	"""
 
+	for key, value in GENE_DICTIONARY.items():
+		print("8: ", key, ": ", value)
+		break;
+
 	index = 0
 
 	for key, value in GENE_DICTIONARY.items():
 		
 		GENE_DICTIONARY[key] = COMBINED[index]
 		index += 1
+
+	for key, value in GENE_DICTIONARY.items():
+		print("9: ", key, ": ", value)
+		break;
 
 	return GENE_DICTIONARY

programming/simulation_CIBERSORT/quantile_normalisation.py
@@ -25,11 +25,18 @@ def algo(GENE_VALUES_MATRIX):
 	""" Quantile normalization for mean value between to .CEL files.
 	Calculate vectors, create matrix based on vectors and normalize the matrix with quantile normalisation.
 	"""
-
+	print("3: ", GENE_VALUES_MATRIX[0])
 	preprocessCore = importr('preprocessCore')
 	vector = robjects.FloatVector([ element for column in GENE_VALUES_MATRIX for element in column ])
+	print("4: ", vector[0])
+	print("4: ", vector[1])
 	matrix = robjects.r['matrix'](vector, ncol = len(GENE_VALUES_MATRIX[0]), byrow=False)
+	print("5: ", matrix[0])
+	print("5: ", matrix[1])
 	R_normalized_matrix = preprocessCore.normalize_quantiles(matrix)
+	print("6: ", R_normalized_matrix[0])
+	print("6: ", R_normalized_matrix[1])
 	normalized_matrix = np.array(R_normalized_matrix)
-	
+	print("7: ", normalized_matrix[0])
+	print("7: ", normalized_matrix[1])
 	return normalized_matrix