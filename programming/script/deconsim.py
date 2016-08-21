171540a27a160d667863daba5936ad5f40988dcb
Jørgen <jorgenjr11@gmail.com>
2016-08-21 19:07:37 +0200

    changed blackbox.py to deconsim.py and updated README.

--- programming/script/deconsim.py
+++ programming/script/deconsim.py
@@ -7,12 +7,6 @@ import argparse
 import config
 
 
-FLAGS = []
-MIXTURES = []
-TUMORS = []
-CELL_LINES = []
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--MIXTURES", help="Mixtures", nargs='*')
 parser.add_argument("-t", "--TUMORS", help="Tumors", nargs='*')
@@ -64,18 +58,18 @@ def execute_args():
 	# stop = timeit.default_timer()
 	# print("Time spent: %.2f seconds" % (stop - start))
 
-	print("Conversion completed! Executing CIBERSORT ... ")
-	files_done = 0
-	start = timeit.default_timer()
-	for tumor_content in range(config.START_TUMOR, config.STOP_TUMOR, config.STEP_TUMOR):
-		for noise_content in range(config.START_NOISE, config.STOP_NOISE, config.STEP_NOISE):
-			# cmd = "java -Xmx3g -Xms3g -jar " + config.PATH_CIBERSORT + " -M " + config.CIBERSORT_MIXTURES + str(tumor_content) + "_" + str(noise_content) + " -c " + config.PHENOTYPE_CLASSES_FILE + " -P " + config.REFERENCE_FILE + " > " + config.CIBERSORT_OUTPUT + str(tumor_content) + "_" + str(noise_content)
-			cmd = "Rscript " + config.PATH_CIBERSORT_R + " " + config.PATH + "Master_files/external/GSE11103_matrix_classes.GSE11103-GSE10650.AbbasPure.mas5.bm.K999.0.txt " + config.CIBERSORT_MIXTURES + str(tumor_content) + "_" + str(noise_content) + " " + config.CIBERSORT_R_OUTPUT + str(tumor_content) + "_" + str(noise_content)
-			os.system(cmd)
-			files_done += 1
-			print("--- CIBERSORT is done with " + str(files_done) + " files.")
-	stop = timeit.default_timer()
-	print("Time spent: %.2f seconds" % (stop - start))
+	# print("Conversion completed! Executing CIBERSORT ... ")
+	# files_done = 0
+	# start = timeit.default_timer()
+	# for tumor_content in range(config.START_TUMOR, config.STOP_TUMOR, config.STEP_TUMOR):
+	# 	for noise_content in range(config.START_NOISE, config.STOP_NOISE, config.STEP_NOISE):
+	# 		# cmd = "java -Xmx3g -Xms3g -jar " + config.PATH_CIBERSORT + " -M " + config.CIBERSORT_MIXTURES + str(tumor_content) + "_" + str(noise_content) + " -c " + config.PHENOTYPE_CLASSES_FILE + " -P " + config.REFERENCE_FILE + " > " + config.CIBERSORT_OUTPUT + str(tumor_content) + "_" + str(noise_content)
+	# 		cmd = "Rscript " + config.PATH_CIBERSORT_R + " " + config.PATH + "Master_files/external/GSE11103_matrix_classes.GSE11103-GSE10650.AbbasPure.mas5.bm.K999.0.txt " + config.CIBERSORT_MIXTURES + str(tumor_content) + "_" + str(noise_content) + " " + config.CIBERSORT_R_OUTPUT + str(tumor_content) + "_" + str(noise_content)
+	# 		os.system(cmd)
+	# 		files_done += 1
+	# 		print("--- CIBERSORT is done with " + str(files_done) + " files.")
+	# stop = timeit.default_timer()
+	# print("Time spent: %.2f seconds" % (stop - start))
 
 	# print("CIBERSORT completed! Exexuting LLSR ... ")
 	# files_done = 0
@@ -90,17 +84,17 @@ def execute_args():
 	# stop = timeit.default_timer()
 	# print("Time spent: %.2f seconds" % (stop - start))
 
-	print("LLSR completed! Voting ... ")
-	files_done = 0
-	start = timeit.default_timer()
-	for tumor_content in range(config.START_TUMOR, config.STOP_TUMOR, config.STEP_TUMOR):
-		for noise_content in range(config.START_NOISE, config.STOP_NOISE, config.STEP_NOISE):
-			cmd = "python3 " + config.PATH + "Master/programming/script/voting.py -i " + str(tumor_content) + " " + str(noise_content)
-			os.system(cmd)
-			files_done += 1
-			print("--- Voting is done with " + str(files_done) + " files.")
-	stop = timeit.default_timer()
-	print("Time spent: %.2f seconds" % (stop - start))
+	# print("LLSR completed! Voting ... ")
+	# files_done = 0
+	# start = timeit.default_timer()
+	# for tumor_content in range(config.START_TUMOR, config.STOP_TUMOR, config.STEP_TUMOR):
+	# 	for noise_content in range(config.START_NOISE, config.STOP_NOISE, config.STEP_NOISE):
+	# 		cmd = "python3 " + config.PATH + "Master/programming/script/voting.py -i " + str(tumor_content) + " " + str(noise_content)
+	# 		os.system(cmd)
+	# 		files_done += 1
+	# 		print("--- Voting is done with " + str(files_done) + " files.")
+	# stop = timeit.default_timer()
+	# print("Time spent: %.2f seconds" % (stop - start))
 
 	print("Voting completed!")
 
