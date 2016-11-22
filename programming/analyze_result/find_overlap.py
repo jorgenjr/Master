86f419043172218a949fa6edc28bb2428ae4ef1e
jorgen <jorgen@meshcrafts.com>
2016-11-22 21:38:58 +0100

    Finding overlapping genes for custom LM signature matrix

--- programming/analyze_result/find_overlap.py
+++ programming/analyze_result/find_overlap.py
@@ -91,4 +91,147 @@ def find_overlap():
 	f_4.close()
 
 
-find_overlap()
+def find_overlap_custom_LM():
+
+	f_1 = open('../../../Master_files/external/GSE22886_new.txt', 'r')
+	f_2 = open('../../../Master_files/external/GSE4527_new.txt', 'r')
+	f_3 = open('../../../Master_files/external/GSE5099_new.txt', 'r')
+	f_4 = open('../../../Master_files/external/GSE22886_overlap.txt', 'w')
+	f_5 = open('../../../Master_files/external/GSE4527_overlap.txt', 'w')
+	f_6 = open('../../../Master_files/external/GSE5099_overlap.txt', 'w')
+	f_7 = open('../../../Master_files/external/GSE26495_PD1lowhigh.txt', 'r')
+	f_8 = open('../../../Master_files/external/GSE26495_PD1lowhigh_overlap.txt', 'w')	
+
+	GSE22886 = []
+	GSE4527 = []
+	GSE5099 = []
+	GSE26495 = []
+	header = True
+
+	for line in f_1:
+
+		if header == True:
+			f_4.write(line)
+			header = False
+			continue
+
+		temp_array = []
+		splitted_line = line.split('\t')
+		splitted_line[0] = splitted_line[0].split("\"")[1]
+
+		for i in range(len(splitted_line)):
+			temp_array.append(splitted_line[i])
+
+		GSE22886.append(temp_array)
+
+	header = True
+
+	for line in f_2:
+
+		if header == True:
+			f_5.write(line)
+			header = False
+			continue
+
+		temp_array = []
+		splitted_line = line.split('\t')
+		splitted_line[0] = splitted_line[0].split("\"")[1]
+
+		for i in range(len(splitted_line)):
+			temp_array.append(splitted_line[i])
+
+		GSE4527.append(temp_array)
+
+	header = True
+
+	for line in f_3:
+
+		if header == True:
+			f_6.write(line)
+			header = False
+			continue
+
+		temp_array = []
+		splitted_line = line.split('\t')
+		splitted_line[0] = splitted_line[0].split("\"")[1]
+
+		for i in range(len(splitted_line)):
+			temp_array.append(splitted_line[i])
+
+		GSE5099.append(temp_array)
+
+	header = True
+
+	for line in f_7:
+
+		if header == True:
+			f_8.write(line)
+			header = False
+			continue
+
+		temp_array = []
+		splitted_line = line.split('\t')
+
+		for i in range(len(splitted_line)):
+			temp_array.append(splitted_line[i])
+
+		GSE26495.append(temp_array)
+
+	print("*** GSE22886 ***")
+	index = 0
+
+	for i in range(len(GSE22886)):
+		
+		found1 = []
+		found2 = []
+		found3 = []
+
+		for j in range(len(GSE4527)):
+			if GSE22886[i][0] == GSE4527[j][0]:
+				found1 = GSE4527[j]
+				break
+
+		for j in range(len(GSE5099)):
+			if GSE22886[i][0] == GSE5099[j][0]:
+				found2 = GSE5099[j]
+				break
+
+		for j in range(len(GSE26495)):
+			if GSE22886[i][0] == GSE26495[j][0]:
+				found3 = GSE26495[j]
+				break
+
+		if len(found1) == 0 or len(found2) == 0 or len(found3) == 0:
+			index += 1
+		else:
+			for k in range(len(GSE22886[i])):
+				f_4.write(GSE22886[i][k])
+				if k != len(GSE22886[i]) - 1:
+					f_4.write('\t')
+			for k in range(len(GSE4527[i])):
+				f_5.write(GSE4527[i][k])
+				if k != len(GSE4527[i]) - 1:
+					f_5.write('\t')
+			for k in range(len(GSE5099[i])):
+				f_6.write(GSE5099[i][k])
+				if k != len(GSE5099[i]) - 1:
+					f_6.write('\t')
+			for k in range(len(GSE26495[i])):
+				f_8.write(GSE26495[i][k])
+				if k != len(GSE26495[i]) - 1:
+					f_8.write('\t')
+
+	print(index)
+	
+	f_1.close()
+	f_2.close()
+	f_3.close()
+	f_4.close()
+	f_5.close()
+	f_6.close()
+	f_7.close()
+	f_8.close()
+
+
+# find_overlap()
+find_overlap_custom_LM()
--- programming/analyze_result/separate.py
+++ programming/analyze_result/separate.py
@@ -3,9 +3,9 @@ import linecache
 
 # Variables for GSE11103
 
-BEGIN = 64
-END = 54739
-EOF = 54740
+BEGIN = 1
+END = 22284
+EOF = 22285
 FILECOLS = 42
 
 
@@ -101,5 +101,244 @@ def separate_mixtures():
 	f.close();
 
 
+def get_relevant_cells():
+
+	fr = open('../../../Master_files/external/GSE22886.txt', 'r')
+	fw = open('../../../Master_files/external/GSE22886_new.txt', 'w')
+
+	Tcd8Start = 1
+	Tcd8End = 5
+	Tcd4ResStart = 19
+	Tcd4ResEnd = 22
+	Tcd4ActStart = 22
+	Tcd4ActEnd = 25
+	NKResStart = 25
+	NKResEnd = 29
+	NKActStart = 29
+	NKActEnd = 40
+	BnaiveStart = 40
+	BnaiveEnd = 47
+	BmemoryStart = 47
+	BmemoryEnd = 55
+	PlasmaStart = 55
+	PlasmaEnd = 62
+	MonoStart = 62
+	MonoEnd = 74
+	MacM0Start = 86
+	MacM0End = 98
+	DenResStart = 98
+	DenResEnd = 104
+	DenActStart = 104
+	DenActEnd = 110
+	NeuStart = 110
+	NeuEnd = 115
+
+	Header = True
+
+	for line in fr:
+
+		splitted_line = line.split('\t')
+		fw.write(splitted_line[0])
+
+		# T cells CD8 GSM565269 GSM565270 GSM565271 GSM565272
+		for i in range(Tcd8Start, Tcd8End):
+			if Header == True:
+				fw.write("\tT cells CD8")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# T cells CD4 memory resting GSM565287 GSM565288 GSM565289
+		for i in range(Tcd4ResStart, Tcd4ResEnd):
+			if Header == True:
+				fw.write("\tT cells CD4 memory resting")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# T cells CD4 memory activated GSM565290 GSM565291 GSM565292
+		for i in range(Tcd4ActStart, Tcd4ActEnd):
+			if Header == True:
+				fw.write("\tT cells CD4 memory actived")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# NK cells resting GSM565293 GSM565294 GSM565295 GSM565296
+		for i in range(NKResStart, NKResEnd):
+			if Header == True:
+				fw.write("\tNK cells resting")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# NK cells activated GSM565297 GSM565298 GSM565299 GSM565300 GSM565301 GSM565302 GSM565303 GSM565304 GSM565305 GSM565306 GSM565307
+		for i in range(NKActStart, NKActEnd):
+			if Header == True:
+				fw.write("\tNK cells activated")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# B cells naïve GSM565308 GSM565309 GSM565310 GSM565311 GSM565312 GSM565313 GSM565314
+		for i in range(BnaiveStart, BnaiveEnd):
+			if Header == True:
+				fw.write("\tB cells naïve")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# B cells memory GSM565315 GSM565316 GSM565317 GSM565318 GSM565319 GSM565320 GSM565321 GSM565322
+		for i in range(BmemoryStart, BmemoryEnd):
+			if Header == True:
+				fw.write("\tB cells memory")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# Plasma cells GSM565323 GSM565324 GSM565325 GSM565326 GSM565327 GSM565328 GSM565329
+		for i in range(PlasmaStart, PlasmaEnd):
+			if Header == True:
+				fw.write("\tPlasma cells")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# Monocytes GSM565330 GSM565331 GSM565332 GSM565333 GSM565334 GSM565335 GSM565336 GSM565337 GSM565338 GSM565339 GSM565340 GSM565341
+		for i in range(MonoStart, MonoEnd):
+			if Header == True:
+				fw.write("\tMonocytes")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# Macrophages M0 GSM565354 GSM565355 GSM565356 GSM565357 GSM565358 GSM565359 GSM565360 GSM565361 GSM565362 GSM565363 GSM565364 GSM565365
+		for i in range(MacM0Start, MacM0End):
+			if Header == True:
+				fw.write("\tMacrophages M0")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# Dendritic cells resting GSM565366 GSM565367 GSM565368 GSM565369 GSM565370 GSM565371
+		for i in range(DenResStart, DenResEnd):
+			if Header == True:
+				fw.write("\tDendritic cells resting")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# Dendritic cells activated GSM565372 GSM565373 GSM565374 GSM565375 GSM565376 GSM565377
+		for i in range(DenActStart, DenActEnd):
+			if Header == True:
+				fw.write("\tDendritic cells activated")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# Neutrophils GSM565378 GSM565379 GSM565380 GSM565381 GSM565382
+		for i in range(NeuStart, NeuEnd):
+			if Header == True:
+				fw.write("\tNeutophils")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		if Header == True:
+			fw.write("\n")
+			Header = False
+		# break
+
+	fr.close()
+	fw.close()
+
+	fr = open('../../../Master_files/external/GSE4527.txt', 'r')
+	fw = open('../../../Master_files/external/GSE4527_new.txt', 'w')
+
+	Treg1 = 2
+	Treg2 = 4
+
+	Header = True
+
+	for line in fr:
+
+		splitted_line = line.split('\t')
+		fw.write(splitted_line[0])
+
+		# T cells regulatory (Tregs) GSM101519 GSM101521
+		if Header == True:
+			fw.write("\tT cells regulatory\tT cells regulatory\n")
+		else:
+			fw.write("\t" + splitted_line[Treg1] + "\t" + splitted_line[Treg2] + "\n")
+
+		if Header == True:
+			Header = False
+
+	fr.close()
+	fw.close()
+
+	fr = open('../../../Master_files/external/GSE5099.txt', 'r')
+	fw = open('../../../Master_files/external/GSE5099_new.txt', 'w')
+
+	M1Start = 10
+	M1End = 13
+	M2Start = 13
+	M2End = 16
+
+	Header = True
+
+	for line in fr:
+
+		splitted_line = line.split('\t')
+		fw.write(splitted_line[0])
+
+		# Macrophages M1 GSM115055 GSM115056 GSM115057
+		for i in range(M1Start, M1End):
+			if Header == True:
+				fw.write("\tMacrophages M1")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# Macrophages M2 GSM115058 GSM115059 GSM115060
+		for i in range(M2Start, M2End):
+			if Header == True:
+				fw.write("\tMacrophages M2")
+			else:
+				fw.write("\t" + splitted_line[Treg1])
+
+		fw.write("\n")
+
+		if Header == True:
+			Header = False
+
+	fr.close()
+	fw.close()
+
+	fr = open('../../../Master_files/external/GSE26495_new.txt', 'r')
+	fw = open('../../../Master_files/external/GSE26495_PD1lowhigh.txt', 'w')
+
+	highStart = 5
+	highEnd = 11
+	lowStart = 11
+	lowEnd = 17
+
+	Header = True
+
+	for line in fr:
+
+		splitted_line = line.split('\t')
+		fw.write(splitted_line[0])
+
+		# PD-1 high
+		for i in range(highStart, highEnd):
+			if Header == True:
+				fw.write("\tPD-1 high")
+			else:
+				fw.write("\t" + splitted_line[i])
+
+		# PD-1 low
+		for i in range(lowStart, lowEnd):
+			if Header == True:
+				fw.write("\tPD-1 low")
+			else:
+				fw.write("\t" + splitted_line[Treg1])
+
+		fw.write("\n")
+
+		if Header == True:
+			Header = False
+
+	fr.close()
+	fw.close()
+
+
 #read_files()
-separate_mixtures()
+#separate_mixtures()
+get_relevant_cells()
